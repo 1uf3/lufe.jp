2350f9dfb3a1ee72016a15ab3086c33bef686ce6
lufe <lufe@lufesMac.local>
2021-07-13 05:33:12 +0900

    github action

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout title="Home">
-
-      <p className="text-4xl">Welcome to lufe's Site</p>
-
-      </Layout>
-  )
+      <p className="text-4xl">Welcome to lufes Site</p>
+    </Layout>
+  );
 }
