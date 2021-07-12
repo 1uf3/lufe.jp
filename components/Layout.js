e9e0ace26d3eb2efd172ecd63774638fe3441674
lufe <lufe@lufesMac.local>
2021-07-13 06:45:48 +0900

    [feat] contact-page

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from 'next/image';
 
 export default function Layout({ children, title = "Default Layout"}) {
     return (
