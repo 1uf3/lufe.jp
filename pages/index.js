2821c987cf34adafa47cfa9394dd5b5638e9467c
lufe <lufe@lufesMac.local>
2021-07-13 15:17:08 +0900

    Add animation HP

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout title="Home">
-      <p className="text-4xl">Welcome to lufes Site</p>
+      <p className="text-6xl animate-pulse">Welcome to lufes Site</p>
     </Layout>
   );
 }
