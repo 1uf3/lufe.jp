9e6e62fa34857b32857defc1142887d69cb1a465
lufe <lufe@lufesMac.local>
2021-07-13 14:57:17 +0900

    Template Blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,20 @@
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-export default function Blog () {
-    return (
-        <Layout title = "Blog">Blog page</Layout>
-    );
+export default function Blog({ posts }) {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
 }
