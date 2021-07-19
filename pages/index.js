8f52cc3ca8c6f76d8485c81e551f70908aaf466f
lufe <lufe@lufesMac.local>
2021-07-20 04:01:52 +0900

    Responsive Home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout title="Home">
-      <p className="text-6xl animate-pulse">Welcome to lufes Site</p>
+      <p className="text-2xl lg:text-6xl animate-pulse">Welcome to lufes Site</p>
     </Layout>
   );
 }
