638bf0b10592597fc7b251584e02fab54be46089
lufe <contact@lufe.jp>
2021-10-14 22:40:14 +0900

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -34,8 +34,16 @@ export default function Profile () {
               <td className="px-2 py-2">Security Camp 全国大会 2021 : D Track Tutor</td>
             </tr>
             <tr className="border-b text-lg">
+              <td className="px-2 py-2">2021/10 - 2021/11</td>
+              <td className="px-2 py-2">Mini Security Camp 2021 : Student & Tutor</td>
+            </tr>
+            <tr className="border-b text-lg">
               <td className="px-2 py-2">2021/5 - Present</td>
               <td className="px-2 py-2">SecHack365 2021 表現駆動 : Trainee</td>
+            </tr>
+            <tr className="border-b text-lg">
+              <td className="px-2 py-2">2021/7 - Present</td>
+              <td className="px-2 py-2">脆弱性診断士</td>
             </tr>
            
 
